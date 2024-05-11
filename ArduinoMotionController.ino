8beb72d8d92e28297874835222fff6921c6dd3cc
Luky Drum <lukbubi12@gmail.com>
2024-05-11 20:05:08 +0200

    Add reading the state of buttons

--- ArduinoMotionController.ino
+++ ArduinoMotionController.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LEFT_BUTTON 12
+#define RESET_BUTTON 11
+#define RIGHT_BUTTON 10
 
+#define LOOP_TIME 20
+
+// Struct holding information about the current state of the controller
+struct ControllerInfo {
+  bool leftButton;
+  bool rightButton;
+};
+
+// Setup all pin modes
+void setPinModes() {
+  pinMode(LEFT_BUTTON, INPUT_PULLUP);
+  pinMode(RESET_BUTTON, INPUT_PULLUP);
+  pinMode(RIGHT_BUTTON, INPUT_PULLUP);
+}
+
+void setControllerInfo(ControllerInfo * cInfo) {
+  cInfo->leftButton = digitalRead(LEFT_BUTTON) == LOW;
+  cInfo->rightButton = digitalRead(RIGHT_BUTTON) == LOW;
+}
+
+void outputControllerInfo(ControllerInfo * cInfo) {
+  Serial.print("L: ");
+  Serial.print(cInfo->leftButton);
+  Serial.print(", R: ");
+  Serial.print(cInfo->rightButton);
+  Serial.println();
+}
+
+// Init controller info
+ControllerInfo controllerInfo = {false, false};
+
+void setup() {
+  setPinModes();
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  setControllerInfo(&controllerInfo);
 
+  outputControllerInfo(&controllerInfo);
+
+  delay(LOOP_TIME);
 }
